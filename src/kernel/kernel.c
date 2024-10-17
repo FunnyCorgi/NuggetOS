d140bfad3ed643b4a1e2f45700def459bd12eacd
FunnyCorgi <166766151+FunnyCorgi@users.noreply.github.com>
2024-10-17 21:44:37 +0000

    make it say "Hello!" from the kernel(this will also test if i need to ++ twice or not to go to the next position)

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,6 +1,20 @@
 // #include "drivers/screen.h"
 void main(){
     char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
-    
+    *video_memory = 'H';
+    video_memory++;
+    video_memory++;
+    *video_memory = 'e';
+    video_memory++;
+    video_memory++;
+    *video_memory = 'l';
+    video_memory++;
+    video_memory++;
+    *video_memory = 'l';
+    video_memory++;
+    video_memory++;
+    *video_memory = 'o';
+    video_memory++;
+    video_memory++;
+    *video_memory = '!';
 }
